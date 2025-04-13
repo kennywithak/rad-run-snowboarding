7dcccb6701f6c7251a7ecae7b477c190d5580557
kennywithak <nelxenny@gmail.com>
2025-04-13 19:55:48 -0400

    updated 4.13.25

--- smart contracts/src/state.rs
+++ smart contracts/src/state.rs
@@ -1,19 +1,19 @@
 use linera_sdk::base::Owner;
-use linera_views::{common::Context, views::View, views::MapView};
+use linera_views::{context::Context, map_view::MapView, views::View};
 use serde::{Deserialize, Serialize};
 use linera_views::views::{ViewError, View};
 
 #[derive(Debug, Serialize, Deserialize, View)]
-pub struct RadRunScores {
-    pub scores: MapView<Owner, u64>,
+pub struct RadRunScores<C> {
+    pub scores: MapView<C, Owner, u64>,
 }
 
-impl RadRunScores {
-    pub fn new(context: &Context) -> Self {
+impl<C> RadRunScores<C> {
+    pub fn new(context: C) -> Self {
         Self {
-            scores: MapView::new(context.clone()),
+            scores: MapView::new(context),
         }
     }
 }
 
-pub type RadRunScoresState = RadRunScores;
+pub type RadRunScoresState = RadRunScores<Context>;
